01b827ade4dbff8edd10fcba3a273376a72748f9
Duncan McGreggor <oubiwann@gmail.com>
2018-09-03 08:32:52 -0500

    Split out graphdb and db protocols.

---
+++ src/hxgm30/db/plugin/protocols/db.clj
@@ -0,0 +1,13 @@
+(defprotocol DBAPI
+  (backup [this] [this path])
+  (closed? [this])
+  (commit [this])
+  (configuration [this])
+  (disconnect [this])
+  (dump [this])
+  (explain [this query-str])
+  (flush [this])
+  (open? [this])
+  (rollback [this])
+  (show-features [this])
+  (tx [this]))
