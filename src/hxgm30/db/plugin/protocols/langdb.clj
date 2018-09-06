61a744ffa9910938b08a809e9afae9d05c17d1d2
Duncan McGreggor <oubiwann@gmail.com>
2018-09-06 01:04:23 -0500

    Added generator type to stats methods.

--- src/hxgm30/db/plugin/protocols/langdb.clj
+++ src/hxgm30/db/plugin/protocols/langdb.clj
@@ -1,7 +1,7 @@
 (defprotocol LangDBAPI
-  (ingest-stats [this language] [this race-name name-type])
-  (lang-stats [this language])
-  (name-stats [this race-name name-type])
+  (ingest-stats [this language] [this race-name name-type generator-type])
+  (lang-stats [this language generator-type])
+  (name-stats [this race-name name-type generator-type])
   ;; Note that pos-tag (part-of-speech tag) is one of the tags defined by the
   ;; Penn Treebank Project. For a full listing, see:
   ;;  * https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
