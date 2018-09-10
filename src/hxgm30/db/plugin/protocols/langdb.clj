8c603f03c0d1e29686f6a05424f6cfb36e5ada59
Duncan McGreggor <oubiwann@gmail.com>
2018-09-09 21:53:40 -0500

    Added a new method to the land db protocol.

--- src/hxgm30/db/plugin/protocols/langdb.clj
+++ src/hxgm30/db/plugin/protocols/langdb.clj
@@ -4,6 +4,8 @@
                 [this race-name name-type generator-type data])
   (lang-stats [this language generator-type])
   (name-stats [this race-name name-type generator-type])
+  (get-stats [this language generator-type]
+             [this race-name name-type generator-type])
   ;; Note that pos-tag (part-of-speech tag) is one of the tags defined by the
   ;; Penn Treebank Project. For a full listing, see:
   ;;  * https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
