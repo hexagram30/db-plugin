8999eaf1c6dc6d8edb2d5aa82cdc93338387ab23
Duncan McGreggor <oubiwann@gmail.com>
2018-10-07 12:04:55 -0500

    Added placeholder dosctrings to protocol methods.

--- src/hxgm30/db/plugin/protocols/db.clj
+++ src/hxgm30/db/plugin/protocols/db.clj
@@ -1,12 +1,23 @@
 (defprotocol DBAPI
-  (backup [this] [this path])
-  (closed? [this])
-  (commit [this])
-  (configuration [this])
-  (disconnect [this])
-  (dump [this])
-  (explain [this query-str])
-  (flush [this])
-  (open? [this])
-  (rollback [this])
-  (tx [this]))
+  (backup [this] [this path]
+    "")
+  (closed? [this]
+    "")
+  (commit [this]
+    "")
+  (configuration [this]
+    "")
+  (disconnect [this]
+    "")
+  (dump [this]
+    "")
+  (explain [this query-str]
+    "")
+  (flush [this]
+    "")
+  (open? [this]
+    "")
+  (rollback [this]
+    "")
+  (tx [this]
+    ""))
